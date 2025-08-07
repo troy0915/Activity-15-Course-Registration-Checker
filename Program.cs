148adffd5c9575e8052d864ddce8279965a46eae
troy0915 <johntroydolom7@gmail.com>
2025-08-07 15:27:46 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_15_Course_Registration_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> courses = new List<string>(6);
+            int attempts = 0;
+            const int maxCourses = 6;
+
+            Console.WriteLine("Course Registration System");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine($"Please register up to {maxCourses} courses");
+            Console.WriteLine("Each course code must be exactly 5 characters long");
+            Console.WriteLine("Duplicate course codes are not allowed\n");
+
+            while (attempts < maxCourses)
+            {
+                Console.Write($"Enter course code #{attempts + 1}: ");
+                string input = Console.ReadLine();
+                if (input.Length != 5)
+                {
+                    Console.WriteLine("Error: Course code must be exactly 5 characters\n");
+                    continue;
+                }
+                if (courses.Contains(input))
+                {
+                    Console.WriteLine("Error: This course code already exists\n");
+                    continue;
+                }
+                courses.Add(input);
+                Console.WriteLine($"Success: Course '{input}' registered\n");
+                attempts++;
+            }
+            Console.WriteLine("\nRegistration Complete!");
+            Console.WriteLine("Registered Courses:");
+            foreach (string course in courses)
+            {
+                Console.WriteLine(course);
+            }
+        }
+    }
+}
+
+
+
+
+
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Activity 15 Course Registration Checker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Activity 15 Course Registration Checker")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5f1e3f12-15f3-445b-aea6-71af241a7b46")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
